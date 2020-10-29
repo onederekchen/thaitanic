e40888b781759a8d6d35f7fb18989c1411d3cb21
Derek Chen <onederekchen@gmail.com>
2020-10-29 09:29:11 -0700

    updated products, added weights

--- generator_test.py
+++ generator_test.py
@@ -15,17 +15,6 @@ def calculate_hourly_weights(weight):
     return (50 + 25 * weight) / 100
 
 
-products = {
-    # product: [price, weight]
-    'Dish 1': [12, 10],
-    'Dish 2': [13, 4],
-    'Dish 3': [13, 8],
-    'Dish 4': [15, 6],
-    'Dish 5': [12, 9],
-    'Dish 6': [13, 2],
-    'Dish 7': [11, 7],
-}
-
 columns = ['Order ID', 'Product', 'Quantity Ordered', 'Price Each', 'Order Date']
 
 years = {  # could add some random variation in here!
@@ -132,16 +121,98 @@ hours_for_day_of_week = {
     6: sunday_hours
 }
 
+starters = {
+    # item name: [price, weight]
+    "Tom Kha Gai": [12, 100],
+    "Sriracha Wings": [10, 1],
+    "Chicken Satay": [10, 1],
+    "Vegan Fresh Rolls": [10, 1],
+}
+
+soup = {
+    # item name: [price, weight]
+    "Tom Yum Gai": [12, 100],
+    "Thai Beef Noodle Soup": [12, 1],
+    "Tom Yum Goong": [15, 1],
+    "Won Ton Soup": [12, 1],
+}
+
+salad = {
+    # item name: [price, weight]
+    "Larb Gai": [12, 100],
+    "Nam Kao Tod": [12, 1],
+    "Yum Woosen": [13, 1],
+    "Som Tum": [12, 1],
+    "Yum Nua": [12, 1],
+    "Waterfall Mushroom and Tofu": [12, 1],
+    "MyThai Salad": [12, 1],
+}
+
+barbecue = {
+    # item name: [price, weight]
+    "Grilled Thai Steak": [20, 100],
+    "Bar-B-Q Chicken": [20, 1],
+    "Grilled Salmon": [22, 1],
+}
+
+sauteed = {
+    # item name: [price, weight]
+    "Pad Gra Prow": [15, 100],
+    "Pad Gra Tiem": [15, 1],
+    "Pad Char": [15, 1],
+}
+
+vegetables = {
+    # item name: [price, weight]
+    "Pad Ma Keur": [12, 100],
+    "Pra Ram": [12, 1],
+    "Pad Pak": [12, 1],
+    "Pad Prig Khing": [12, 1],
+    "Pad Ka Nar": [12, 1],
+}
+
+curries = {
+    # item name: [price, weight]
+    "Green Curry": [12, 100],
+    "Yellow Curry": [12, 1],
+    "Red Curry": [12, 1],
+    "Pumpkin Curry": [15, 1],
+}
+
+noodles_and_rice = {
+    # item name: [price, weight]
+    "Pad Thai": [12, 100],
+    "Pad See Ew": [12, 1],
+    "Thai Fried Rice": [12, 1],
+    "Pad Kee Mao": [12, 1],
+    "Thai Streetfood Noodle": [12, 1],
+    "Basil Fried Rice": [12, 1],
+}
+
+desserts = {
+    "Sticky Rice with Mango": [6, 100],
+}
+
+full_menu = [
+    starters,
+    soup,
+    salad,
+    barbecue,
+    sauteed,
+    vegetables,
+    curries,
+    noodles_and_rice,
+    desserts,
+]
+
+# full_menu_weights order matches full_menu
+full_menu_weights = [3, 3, 4, 2, 5, 5, 8, 10, 3]
 
 # add percent_occupancy to day_hours dictionaries
 print('Calculating weights.')
 for day in hours_for_day_of_week.values():
     for hour in day:
         day[hour].append(calculate_hourly_weights(day.get(hour)[0]))
-
-product_list = [product for product in products]
-product_weights = [products[product][1] for product in products]
-# month_weights = [months[month][1] for month in months]  # probably need to remove this
 
 order_id = 3910
 
@@ -163,21 +234,21 @@ for year in years:
                     order_weight = year_weight * month_weight * hour_weight
                     orders = int(np.random.normal(loc=40*order_weight, scale=5*order_weight))
 
-                    # grab time range for the hour
-
                     for i in range(orders):
                         random_minute = random.randint(0, 59)
 
-                        product = random.choices(product_list, weights=product_weights)[0]
-                        price = products[product][0]
+                        food_type_selection = random.choices(full_menu, weights=full_menu_weights)
+                        food_type_weights = [selection[1] for selection in list(food_type_selection[0].values())]
+                        food_selection = random.choices(list(food_type_selection[0].keys()), food_type_weights)[0]
+
+                        price = food_type_selection[0][food_selection][0]
                         order_date = datetime.datetime(year, month, day, hour, random_minute)
 
                         day_df = pd.DataFrame(columns=columns)
-                        day_df.loc[i] = [order_id, product, 1, price, order_date]
+                        day_df.loc[i] = [order_id, food_selection, 1, price, order_date]
 
                         month_df = month_df.append(day_df, ignore_index=True)
                         order_id += 1
 
-        print()
         month_df.to_csv(f'{months[month][0]}_{year}_test_data.csv')
         print(f"{months[month][0]} {year} complete!")
